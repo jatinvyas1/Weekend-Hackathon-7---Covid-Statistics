cb565a13484dc46475988c9013a7318be191f6de
jatinvyas1 <jatin.vyas@outlook.com>
2021-01-28 13:38:11 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -8,7 +8,12 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 const { connection } = require('./connector')
 
-
+app.get("/totalRecovered",(req,res)=>{
+    connection.aggregate([{$group: {_id:"total", recovered: {$sum:"$recovered"} } }]).then(result=>{
+        console.log(typeof result)
+        res.send({data:result});
+    })
+})
 
 
 
